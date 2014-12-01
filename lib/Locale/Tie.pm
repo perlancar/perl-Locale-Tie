8c2f812b7cea7254921ba05e12aa56e89ff565fc
perlancar (@pc-home) <perlancar@gmail.com>
2014-12-01 11:54:00 +0700

    Update module name

--- lib/Locale/Tie.pm
+++ lib/Locale/Tie.pm
@@ -156,7 +156,7 @@ L<POSIX>
 L<Locale::Scope>
 
 Other modules with the same concept: L<File::chdir>, L<File::umask>,
-L<System::setuid>.
+L<Unix::setuid>.
 
 L<autolocale> which uses L<Variable::Magic> (similar to tie technique) to
 automatically setlocale() when entry C<< $ENV{LANG} >> is set.
